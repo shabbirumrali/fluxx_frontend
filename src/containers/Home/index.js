3e6ccc0bbf89721c545d691c2a8ea136231cf92a
shabbirumrali <shabbirumrali@gmail.com>
2021-03-07 12:09:45 +0530

    Update index.js

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,80 +1,68 @@
 import React from "react";
-import Banner from "../../assets/img/header_image.jpg";
-import Shake from "../../assets/img/handshake.svg";
-import choice from "../../assets/img/choice.svg";
-import lightbulb from "../../assets/img/lightbulb.svg";
+import { Col, Container, Image, Row } from 'react-bootstrap'
+import Walloutlet from "../../assets/img/walloutlet.jpg";
+import Fingersnap from "../../assets/img/fingersnap.png";
+import Guided from "../../assets/img/guided.png";
+import Aha from "../../assets/img/aha.png";
+import Header from "../../components/Header";
+
 const Home = () => {
   return (
     <>
-      <img
-        src={`${Banner}`}
-        className="img-fluid header_image"
-        alt="header_image"
-        style={{
-          width: "100%",
-        }}
-      />
-
-      <div className="container">
-        <div className="row justify-content-md-center">
-          <div className="col-md-auto p-5 my-auto">
-            <h2>why Fluxx ?</h2>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-8 py-md-5">
-            <p className="lead">
-            We like bells and whistles as much as the next person, but in the end,
-            they just make noise. We're building Fluxx with everything you need to
-            get your work done quickly - without all of the extra fluff.
+    <header className="header">
+      <Image src={Walloutlet} fluid />
+      <h2>Projects are hard.<br />Managing them shouldn’t be.</h2>
+    </header>    
+      
+      <Container>
+            <h2 className="text-center my-5">why Fluxx ?</h2>
+        <Row className="py-3 my-5">
+          <Col className="align-self-center">
+            <p>
+              We like bells and whistles as much as the next person, but in the end,
+              they just make noise. We're building Fluxx with everything you need to
+              get your work done quickly - without all of the extra fluff.            
+              <br />
+              If it's in Fluxx, it's because it needs to be.
             </p>
-            <p className="lead">
-            If it's in Fluxx, it's because it needs to be.
-            </p>
-          </div>
-          <div className="col-4 handshake align-self-center">
-            <img src={`${Shake}`} className="img-fluid" alt="handshake" />
-          </div>
-        </div>
-      </div>
+          </Col>
+          <Col>
+            <Image src={Fingersnap} className="mx-auto d-block"/>
+          </Col>
+        </Row>                  
+      </Container>
 
-      <div className="container-fluid bg-light">
-        <div className="row">
-          <div className="col-4 p-5 align-self-center choice_img">
-            <img src={`${choice}`} className="img-fluid" alt="Choice" />
-          </div>
-          <div className="col-8 align-self-center">
-            <p className="lead">
+      <Container fluid>
+        <Row className="bg-light py-5 mt-5">
+          <Col>
+            <Image src={Guided} className="mx-auto d-block"/>
+          </Col>
+
+          <Col className="align-self-center">
+            <p>
               Every great story has a great guide. Frodo had Gandolf, Luke had
               Yoda, Daniel had Mr. Miyagi... you get the picture. Fluxx is
               designed to lead you down the right path, help you when you need
-              it, and guide you (and
+              it, and guide you (and)
             </p>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
 
-      <div className="container-fluid">
-        <div className="row index_last_container p-5">
-          <div className="col-8 align-self-center">
-            <p className="lead">
-              Your company needs visibility into what it's doing to make the
+      <Container fluid style={{ background: "#fbf7f7"}} >
+        <Row className="py-5">
+          <Col className="align-self-center">
+            <p>Your company needs visibility into what it's doing to make the
               right decisions, but doesn't always have the resources for a
               formal project management office. With Fluxx, you'll be able to
               take the first step toward organization and transparency without
-              having to add another branch to your org chart.
-            </p>
-          </div>
-          <div className="col-4 p-5 align-self-center choice_img">
-            <img
-              src={`${lightbulb}`}
-              className="img-fluid"
-              alt="Light_bulb"
-            />
-          </div>
-        </div>
-      </div>
+              having to add another branch to your org chart.</p>
+          </Col>
+          <Col>
+            <Image src={Aha} className="mx-auto d-block" />
+          </Col>
+        </Row>
+      </Container>      
     </>
   );
 };
